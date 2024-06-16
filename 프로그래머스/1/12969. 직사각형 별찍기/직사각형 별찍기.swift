713d2795603e62a8dbb508eefba98ea22c53019c
dbsghdz1 <114382668+dbsghdz1@users.noreply.github.com>
2024-06-16 16:39:05 +0900

    [level 1] Title: 직사각형 별찍기, Time: 8.50 ms, Memory: 14.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+let n = readLine()!.components(separatedBy: " ").map { Int($0)! }
+let (a, b) = (n[0], n[1])
+
+(0..<b).forEach { _ in
+    print(String(repeating: "*", count: a))
+}
+
