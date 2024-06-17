cdab64e7bda5475d5b573dc6672c96543f6dbe14
dbsghdz1 <114382668+dbsghdz1@users.noreply.github.com>
2024-06-17 23:02:31 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.swift
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.swift
@@ -1,3 +1,10 @@
 func solution(_ n:Int64) -> [Int] {
-    return String(n).reversed().map { Int(String($0))! }
+	var n1 = Int(n)
+    var arr: [Int] = [] //크기를 정하지 않고 생성
+    
+    while n1 > 0 {
+    	arr.append(n1 % 10) //10으로 나눈 나머지를 배열에 추가
+        n1 /= 10
+    }
+    return arr
 }
