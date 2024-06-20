ed850d78ee8ea0e0e2bdf1c7b375bf6f59042a63
Hong <114382668+dbsghdz1@users.noreply.github.com>
2024-06-20 11:05:19 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.22 ms, Memory: 17 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+    var answer = Array(repeating: Array(repeating: 0, count: arr2[0].count), count: arr1.count)
+    for i in 0..<answer.count {
+        for j in 0..<answer[0].count {
+            answer[i][j] = 0
+        }
+    }
+    for i in 0..<arr1.count {
+        for j in 0..<arr2[0].count {
+            var sum = 0
+            for k in 0..<arr2.count {
+                sum += arr1[i][k] * arr2[k][j]
+            }
+            answer[i][j] = sum
+        }
+    }
+    return answer
+}
