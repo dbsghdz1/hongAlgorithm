e0bff3e95508da682edac2256a43e4ed894e0ef9
Hong <114382668+dbsghdz1@users.noreply.github.com>
2024-06-25 19:20:19 +0900

    [level 2] Title: 피로도, Time: 0.19 ms, Memory: 16.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.swift
@@ -0,0 +1,46 @@
+import Foundation
+
+func solution(_ k: Int, _ dungeons: [[Int]]) -> Int {
+  var maxVal = 0
+  
+  func permutations(_ nums: [[Int]], _ arr: inout [[Int]], _ used: inout [Bool]) {
+    if arr.count == nums.count {
+      maxVal = max(maxVal, check(k, arr))
+      return
+    }
+    
+    for i in 0..<nums.count {
+      if used[i] {
+        continue
+      }
+      arr.append(nums[i])
+      used[i] = true
+      permutations(nums, &arr, &used)
+      arr.removeLast()
+      used[i] = false
+    }
+  }
+  
+  func check(_ k: Int, _ dungeons: [[Int]]) -> Int {
+    var blood = k
+    var cnt = 0
+    for dungeon in dungeons {
+      let first = dungeon[0]
+      let second = dungeon[1]
+      if blood >= first {
+        blood -= second
+        cnt += 1
+      } else {
+        break
+      }
+    }
+    return cnt
+  }
+  
+  var arr: [[Int]] = []
+  var used: [Bool] = Array(repeating: false, count: dungeons.count)
+  permutations(dungeons, &arr, &used)
+  
+  return maxVal
+}
+
