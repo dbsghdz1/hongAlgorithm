8e24e91803b8021d15bc24316c799f2c9a89dd0c
Hong <114382668+dbsghdz1@users.noreply.github.com>
2024-06-30 22:46:54 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 1.27 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.swift
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.swift
@@ -1,9 +1,25 @@
 func solution(_ strings:[String], _ n:Int) -> [String] {
-    var dict: [Character : String] = [:]
+    var dict: [Character : [String]] = [:]
+    var answer = [String]()
+    for i in strings {
+       var arr = Array(i)
+        var key = arr[n]
+        dict[key] = []
+    }
     for i in strings {
         var arr = Array(i)
-        dict[arr[n]] = i
+        var key = arr[n]
+        print(key, i)
+        if var array = dict[key] {
+            array.append(i)
+            dict[key] = array.sorted()
+         }
     }
     print(dict)
-    return []
+    for i in dict.keys.sorted() {
+         for j in dict[i]! {
+              answer.append(j)
+         }
+    }
+    return answer
 }
