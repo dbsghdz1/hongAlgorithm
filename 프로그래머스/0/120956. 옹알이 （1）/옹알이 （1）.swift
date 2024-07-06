2497ff9b679167383ebd2b00e66d0f2b40839a1d
Hong <114382668+dbsghdz1@users.noreply.github.com>
2024-07-07 01:45:42 +0900

    [level 0] Title: 옹알이 (1), Time: 0.07 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.swift
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.swift
@@ -1,19 +1,21 @@
 import Foundation
 
 func solution(_ babbling:[String]) -> Int {
-    var baby = ["aya", "ye", "woo", "ma"]
+    let baby = ["aya", "ye", "woo", "ma"]
     var answer = 0
-    for s in babbling {
-        var check = false
-        var cnt = 0
-        for i in 0...3 {
-            if s.contains(baby[i]) {
-                cnt += baby[i].count
+    for i in babbling {
+        var spelling = ""
+        for j in i {
+            spelling += String(j) 
+            for i in baby {
+                if i == spelling {
+                    spelling = ""
+                }
             }
         }
-        if cnt == s.count {
-            answer += 1
-        }
+        if spelling.isEmpty {
+                answer += 1
+            }
     }
     return answer
 }
