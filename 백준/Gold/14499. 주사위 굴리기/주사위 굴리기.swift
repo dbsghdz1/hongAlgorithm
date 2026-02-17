60749b9bc8bcc227a543580d33abd19cc67fbfa6
Hong <114382668+dbsghdz1@users.noreply.github.com>
2026-02-18 05:18:22 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 8 ms, Memory: 79520 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.swift
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.swift
@@ -7,11 +7,11 @@ var x = first[2]
 var y = first[3]
 let K = first[4]
 
-var board = [[Int]]()
+var map = [[Int]]()
 
 for _ in 0..<N {
     let row = readLine()!.split(separator: " ").map { Int($0)! }
-    board.append(row)
+    map.append(row)
 }
 
 let commands = readLine()!.split(separator: " ").map { Int($0)! }
@@ -21,46 +21,69 @@ var dx = [0, 0, -1, 1]
 var dy = [1, -1, 0, 0]
 
 for c in commands {
-    let nx = x + dx[c-1]
-    let ny = y + dy[c-1]
-
-    if nx < 0 || nx >= N || ny < 0 || ny >= M {
-        continue
-    }
-
-    x = nx
-    y = ny
     
-    if c == 1 {
-        let temp = dice
-        dice[0] = temp[2]
-        dice[1] = temp[0]
-        dice[5] = temp[1]
-        dice[2] = temp[5]
-    } else if c == 2 {
-        let temp = dice
-        dice[0] = temp[1]
-        dice[2] = temp[0]
-        dice[5] = temp[2]
-        dice[1] = temp[5]
-    } else if c == 3 {
-        let temp = dice
-        dice[0] = temp[4]
-        dice[3] = temp[0]
-        dice[5] = temp[3]
-        dice[4] = temp[5]
-    } else if c == 4 {
-        let temp = dice
-        dice[0] = temp[3]
-        dice[4] = temp[0]
-        dice[5] = temp[4]
-        dice[3] = temp[5]
+    if c == 1 && y + 1 < M {
+        y += 1
+        let d = dice
+        dice[1] = d[0]
+        dice[0] = d[2]
+        dice[2] = d[5]
+        dice[5] = d[1]
+        
+        if map[x][y] == 0 {
+            map[x][y] = dice[5]
+        } else {
+            dice[5] = map[x][y]
+            map[x][y] = 0
+        }
+        print(dice[0])
+        
+    } else if c == 2 && y - 1 >= 0 {
+        y -= 1
+        let d = dice
+        dice[0] = d[1]
+        dice[2] = d[0]
+        dice[5] = d[2]
+        dice[1] = d[5]
+        
+        if map[x][y] == 0 {
+            map[x][y] = dice[5]
+        } else {
+            dice[5] = map[x][y]
+            map[x][y] = 0
+        }
+        print(dice[0])
+        
+    } else if c == 3 && x - 1 >= 0 {
+        x -= 1
+        let d = dice
+        dice[0] = d[4]
+        dice[3] = d[0]
+        dice[5] = d[3]
+        dice[4] = d[5]
+        
+        if map[x][y] == 0 {
+            map[x][y] = dice[5]
+        } else {
+            dice[5] = map[x][y]
+            map[x][y] = 0
+        }
+        print(dice[0])
+        
+    } else if c == 4 && x + 1 < N {
+        x += 1
+        let d = dice
+        dice[4] = d[0]
+        dice[0] = d[3]
+        dice[5] = d[4]
+        dice[3] = d[5]
+        
+        if map[x][y] == 0 {
+            map[x][y] = dice[5]
+        } else {
+            dice[5] = map[x][y]
+            map[x][y] = 0
+        }
+        print(dice[0])
     }
-    if board[x][y] == 0 {
-        board[x][y] = dice[5]   // 바닥 → 칸
-    } else {
-        dice[5] = board[x][y]   // 칸 → 바닥
-        board[x][y] = 0
-    }
-    print(dice[0])
 }
