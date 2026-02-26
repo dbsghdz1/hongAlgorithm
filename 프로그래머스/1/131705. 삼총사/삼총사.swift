cae103fe2760b981c054c82e438a9d24d160ec5b
Hong <114382668+dbsghdz1@users.noreply.github.com>
2026-02-26 10:26:48 +0900

    [level 1] Title: 삼총사, Time: 0.02 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.swift
+++ 프로그래머스/1/131705. 삼총사/삼총사.swift
@@ -1,24 +1,15 @@
 import Foundation
 
 func solution(_ number:[Int]) -> Int {
-    var answer = 0
-    let length = number.count
-    var isSamChongSa = 0
-    
-    for i in 0..<length - 2 {
-        for j in i + 1..<length - 1 {
-            for k in j + 1..<length {
-                isSamChongSa += number[i]
-                isSamChongSa += number[j]
-                isSamChongSa += number[k]
-                print(number[i], number[j], number[k])
-                if isSamChongSa == 0 {
-                    answer += 1
-                } else {
-                    isSamChongSa = 0
-                }
+    var result = 0
+    for a in 0..<number.count {
+        let aIndex = a + 1
+        for b in aIndex..<number.count {
+            let bIndex = b + 1
+            for c in bIndex..<number.count {
+                if number[a] + number[b] + number[c] == 0 { result += 1}
             }
         }
     }
-    return answer
+    return result
 }
