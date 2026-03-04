11cb43b8940a39990f1d4ff7a2f1518a1525705b
Hong <114382668+dbsghdz1@users.noreply.github.com>
2026-03-05 02:17:31 +0900

    [Silver III] Title: 바이러스, Time: 8 ms, Memory: 79516 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.swift
+++ 백준/Silver/2606. 바이러스/바이러스.swift
@@ -6,46 +6,27 @@ let M = Int(readLine()!)!
 var graph = Array(repeating: [Int](), count: N + 1)
 
 for _ in 0..<M {
-    let e = readLine()!.split(separator: " ").map { Int($0)! }
-    let u = e[0]
-    let v = e[1]
-    
-    graph[u].append(v)
-    graph[v].append(u)
+    let edge = readLine()!.split(separator: " ").map { Int($0)! }
+    let a = edge[0]
+    let b = edge[1]
+
+    graph[a].append(b)
+    graph[b].append(a)
 }
 
 var visited = Array(repeating: false, count: N + 1)
-
-func bfs(_ node: Int) {
-    var queue = [Int]()
-    var head = 0
+var answer = 0
+func dfs(_ node: Int) {
+    visited[node] = true
     
-    queue.append(node)
-    
-    while head < queue.count {
-        let cur = queue[head]
-        head += 1
-        
-        if !visited[cur] {
-            visited[cur] = true
-            for g in graph[cur] {
-                if !visited[g] {
-                    queue.append(g)
-                }
-            }
-        } else {
-            continue
+    for next in graph[node] {
+        if !visited[next] {
+            dfs(next)
+            answer += 1
         }
     }
 }
 
-bfs(1)
-var answer = 0
+dfs(1)
 
-for i in visited {
-    if i {
-        answer += 1
-    }
-}
-
-print(answer - 1)
+print(answer)
