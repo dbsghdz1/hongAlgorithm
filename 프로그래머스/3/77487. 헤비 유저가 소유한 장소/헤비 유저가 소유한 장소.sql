102ff33ca97e72ad259c573e9777a43fe6aa1e15
Hong <114382668+dbsghdz1@users.noreply.github.com>
2026-03-04 15:42:34 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,10 +1,18 @@
--- 코드를 입력하세요
-SELECT id, name, host_id
-from places
-where host_id in (
+# select b.id, b.name, b.host_id
+# from places a
+# join places b
+# on a.id = b.id
+# where b.id = a.id 
+# group by a.host_id
+# having count(*) >= 2
+
+select b.id, b.name, b.host_id
+from (
     select host_id
     from places
     group by host_id
     having count(*) >= 2
-)
-order by id
+) a
+join places b
+on a.host_id = b.host_id
+
