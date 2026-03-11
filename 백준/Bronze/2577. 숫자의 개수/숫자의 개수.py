993905089c0fd64fbf007b4cc283d091d487a574
Hong <114382668+dbsghdz1@users.noreply.github.com>
2026-03-11 16:22:58 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -2,14 +2,8 @@ a = int(input())
 b = int(input())
 c = int(input())
 
-sum = a * b * c
+num = str(a*b*c)
 
-arr = [0] * 10
-
-while sum > 0:
-    r = sum % 10
-    sum //= 10
-    arr[r] += 1
 
 for i in range(10):
-    print(f"{arr[i]}")
+    print(num.count(str(i)))
