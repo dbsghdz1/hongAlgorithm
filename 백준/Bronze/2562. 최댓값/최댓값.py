044403004bdc25f72182bc6d0c25329d35792203
Hong <114382668+dbsghdz1@users.noreply.github.com>
2026-03-11 15:52:22 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -1,14 +1,4 @@
-arr = []
+arr = [int(input()) for _ in range(9)]
 
-for _ in range(9):
-    arr.append(int(input()))
-
-max = 0
-index = 0
-for i in range(9):
-    if arr[i] > max:
-        max = arr[i]
-        index = i
-print(max)
-print(index + 1)
-        
+print(max(arr))
+print(arr.index(max(arr)) + 1)
