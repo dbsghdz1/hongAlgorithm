a66b118f2dd59a595957fba88ac8f71f96568b6a
Hong <114382668+dbsghdz1@users.noreply.github.com>
2026-03-31 01:56:06 +0900

    [Silver III] Title: 1로 만들기, Time: 52 ms, Memory: 87324 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+let n = Int(readLine()!)!
+var dp = Array(repeating: 0, count: n + 1)
+
+if n >= 2 {
+    for i in 2...n {
+        dp[i] = dp[i-1] + 1
+        
+        if i % 2 == 0 {
+            dp[i] = min(dp[i], dp[i/2] + 1)
+        }
+        
+        if i % 3 == 0 {
+            dp[i] = min(dp[i], dp[i/3] + 1)
+        }
+    }
+}
+
+print(dp[n])
