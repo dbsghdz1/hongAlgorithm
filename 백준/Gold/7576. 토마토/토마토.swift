3be03e33427cfb9a0de5bc57e9bdfbe65c38f3c2
Hong <114382668+dbsghdz1@users.noreply.github.com>
2026-03-31 15:46:30 +0900

    [Gold V] Title: 토마토, Time: 348 ms, Memory: 120616 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.swift
+++ 백준/Gold/7576. 토마토/토마토.swift
@@ -1,60 +1,63 @@
 import Foundation
 
-let first = readLine()!.split(separator: " ").map { Int($0)! }
-let M = first[0]   // 가로 (열)
-let N = first[1]   // 세로 (행)
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let n = nm[0], m = nm[1]
 
-var box = [[Int]]()
-var queue: [(Int, Int)] = []
-var dist = Array(repeating: Array(repeating: -1, count: M), count: N)
-var head = 0
-let dr = [-1, 1, 0, 0]
-let dc = [0, 0, -1, 1]
-
-for _ in 0..<N {
-    let row = readLine()!.split(separator: " ").map { Int($0)! }
-    box.append(row)
+var grid = (0..<m).map { _ in
+    readLine()!.split(separator: " ").map { Int($0)! }
 }
 
-for r in 0..<N {
-    for c in 0..<M {
-        if box[r][c] == 1 {
-            queue.append((r, c))
-            dist[r][c] = 0
+var dist = Array(repeating: Array(repeating: 0, count: n), count: m)
+var head = 0
+var queue: [(Int, Int)] = []
+
+let dx = [-1, 1, 0, 0]
+let dy = [0, 0, -1, 1]
+
+for (i, g) in grid.enumerated() {
+    for (j, t) in g.enumerated() {
+        if t == 1 {
+            queue.append((i, j))
+            dist[i][j] = 1
         }
     }
 }
 
 while head < queue.count {
-    let (r, c) = queue[head]
+    let (x, y) = queue[head]
     head += 1
     
     for k in 0..<4 {
-        let nr = r + dr[k]
-        let nc = c + dc[k]
+        let nx = x + dx[k]
+        let ny = y + dy[k]
         
-        if nr < 0 || nc < 0 || nr >= N || nc >= M {
-            continue 
-        }
-        if box[nr][nc] != 0 { continue }
-        if dist[nr][nc] != -1 { continue }
+        if nx < 0 || nx >= m || ny < 0 || ny >= n { continue }
+        if grid[nx][ny] != 0 { continue }
+        if dist[nx][ny] != 0 { continue }
         
-        dist[nr][nc] = dist[r][c] + 1
-        box[nr][nc] = 1
-        queue.append((nr, nc))
+        dist[nx][ny] = dist[x][y] + 1
+        queue.append((nx, ny))
     }
 }
 
-var answer = 0
+var max = 0
+var flag = false
 
-for i in 0..<N {
-    for j in 0..<M {
-        if box[i][j] == 0 {
+for i in 0..<m {
+    for j in 0..<n {
+        if grid[i][j] == 0 && dist[i][j] == 0 {
             print(-1)
             exit(0)
         }
-        answer = max(answer, dist[i][j])
     }
 }
 
-print(answer)
+
+    for d in dist {
+        for v in d {
+            if max < v {
+                max = v 
+            }
+        }
+    }
+    print(max - 1)
