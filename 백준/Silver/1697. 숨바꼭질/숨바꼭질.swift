aafd785d5cff57da3cdc586a2f03aface6632e4a
Hong <114382668+dbsghdz1@users.noreply.github.com>
2026-03-31 18:29:31 +0900

    [Silver I] Title: 숨바꼭질, Time: 12 ms, Memory: 71456 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.swift
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.swift
@@ -1,28 +1,38 @@
-import Foundation
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let n = nm[0]
+let m = nm[1]
 
-let input = readLine()!.split(separator: " ").map { Int($0)! }
-let N = input[0]
-let T = input[1]
+var dist = Array(repeating: 0, count: 100001)
 
-var queue: [Int] = [N]
+var queue = [n]
+dist[n] = 1
 var head = 0
-var dist = Array(repeating: -1, count: 100001)
-dist[N] = 0
+
 while head < queue.count {
     let p = queue[head]
     head += 1
     
-    if p == T {
-        print(dist[p])
+    if p == m {
+        print(dist[p] - 1)
         break
     }
     
-    let nexts = [p - 1, p + 1, p * 2]
-    for nx in nexts {
-        if nx < 0 || nx > 100000 { continue }
-        if dist[nx] != -1 { continue }
-        
-        queue.append(nx)
-        dist[nx] = dist[p] + 1
+    let p1 = p - 1
+    let p2 = p + 1
+    let p3 = p * 2
+    
+    if p1 >= 0 && p1 <= 100000 && dist[p1] == 0 {
+        dist[p1] = dist[p] + 1
+        queue.append(p1)
+    }
+    
+    if p2 >= 0 && p2 <= 100000 && dist[p2] == 0 {
+        dist[p2] = dist[p] + 1
+        queue.append(p2)
+    }
+    
+    if p3 >= 0 && p3 <= 100000 && dist[p3] == 0 {
+        dist[p3] = dist[p] + 1
+        queue.append(p3)
     }
 }
